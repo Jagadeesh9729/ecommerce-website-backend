ff1577b64eb07f5262b89ee9a4507527fe07dada
Jagadeesh970 <jagadeeshchowdary344@gmail.com>
2024-05-02 15:12:44 +0530

    ecommerce website

--- server.js
+++ server.js
@@ -2,32 +2,30 @@ import express from "express";
 import dotenv from "dotenv";
 import stripe from "stripe";
 
-// Load Variables
 dotenv.config();
 
-// Start Server
+
 const app = express();
 
 app.use(express.static("public"));
 app.use(express.json());
 
-// Home Route
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: "public" });
 });
-// Cart
+
 app.get("/cart.html", (req, res) => {
   res.sendFile("cart.html", { root: "public" });
 });
-// Success
+
 app.get("/success.html", (req, res) => {
   res.sendFile("success.html", { root: "public" });
 });
-// Success
+
 app.get("/cancel.html", (req, res) => {
   res.sendFile("cancel.html", { root: "public" });
 });
-// Stripe
+
 let stripeGateway = stripe(process.env.stripe_key);
 app.post("/stripe-checkout", async (req, res) => {
   const lineItems = req.body.items.map((item) => {
